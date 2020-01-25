37ede928b14b3b32ac71843ab455184ccd4fbe0e
Anderson Corso <andersoncorso89@gmail.com>
2020-01-25 13:31:45 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/**
+ *  PWA - Service Worker
+ */
+if ('serviceWorker' in navigator) {
+
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        },
+        function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+
+}
